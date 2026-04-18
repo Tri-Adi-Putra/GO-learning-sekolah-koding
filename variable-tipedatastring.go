c7858888c76cc34bfdbead229b23e6da7c8d5d3b
Tri-Adi-Putra <98399147+Tri-Adi-Putra@users.noreply.github.com>
2026-04-19 06:46:06 +0700

    Update variable-tipedatastring.go

--- variable-tipedatastring.go
+++ variable-tipedatastring.go
@@ -2,13 +2,20 @@ package main
 
 import "fmt"
 
+var = " "
+
+var string
+
 var namaDepan = "tri"
 
 var namaBelakang = "putra"
 
 var namaGabungan = namaDepan + " " + namaBelakang
 
+var nama, samping = "agnes", "levia"
+
 func main() {
   fmt.Println("Nama Depan saya" + " " + namaDepan)
   fmt.Println("perkenalkan nama saya" + " " + namaGabungan)
+  fmt.println("Nama Istri Saya" + " " + nama + " " + samping
 }
