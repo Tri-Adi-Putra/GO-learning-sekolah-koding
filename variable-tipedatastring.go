a59cd6771cccb4c45fc6347e44a137718cd7eca0
Tri-Adi-Putra <98399147+Tri-Adi-Putra@users.noreply.github.com>
2026-04-19 06:50:05 +0700

    Update variable-tipedatastring.go

--- variable-tipedatastring.go
+++ variable-tipedatastring.go
@@ -2,20 +2,19 @@ package main
 
 import "fmt"
 
-var = " "
-
-var string
+// var temp = "" // Contoh jika ingin pakai variabel di sini
+// var teks string // Contoh deklarasi variabel string
 
 var namaDepan = "tri"
-
 var namaBelakang = "putra"
-
 var namaGabungan = namaDepan + " " + namaBelakang
 
 var nama, samping = "agnes", "levia"
 
 func main() {
-  fmt.Println("Nama Depan saya" + " " + namaDepan)
-  fmt.Println("perkenalkan nama saya" + " " + namaGabungan)
-  fmt.println("Nama Istri Saya" + " " + nama + " " + samping
+    fmt.Println("Nama Depan saya " + " " + namaDepan)
+    fmt.Println("perkenalkan nama saya " + " " + namaGabungan)
+    
+    // Perhatikan huruf P kapital pada Println
+    fmt.Println("Nama Istri Saya " + " " + nama + " " + samping)
 }
