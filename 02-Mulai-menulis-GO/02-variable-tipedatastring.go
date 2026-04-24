6dcc1e9a8dcbf3cf25cc066791fc2f77cdfaadab
Tri-Adi-Putra <98399147+Tri-Adi-Putra@users.noreply.github.com>
2026-04-24 21:35:38 +0700

    Update 02-variable-tipedatastring.go

--- 02-Mulai-menulis-GO/02-variable-tipedatastring.go
+++ 02-Mulai-menulis-GO/02-variable-tipedatastring.go
@@ -2,28 +2,35 @@ package main
 
 import "fmt"
 
-//tipe data kosong
-var temp = "" 
-var teks string
-
-//penulisan model pertama
 var namaDepan = "tri"
-var namaBelakang = "putra"
-var namaGabungan = namaDepan + " " + namaBelakang
 
-//penulisan model ke dua
-var nama, samping = "agnes", "levia"
+var namaBelakang = "putra"
+
+var namaGabungan = namaDepan + " " + namaBelakang
 
 func main() {
 
-    sekolah := "universita Mercu" //short hand
+	var hasil int //perhitungan latihan modulo
+	a := 32
+	v := 10
+	hasil = a % v
 
+	//cara menaikan nilai angka dan menurunkan
+	r := 10
+	r++
+
+	t := 21
+	t--
+
+	sekolah := "universita Mercu" //short hand
+
+	//tipe data numeric/number
+	umur := 50
+
+	fmt.Println("umur kakek saya ", umur)
 	fmt.Println("saya Kuliah di ", sekolah)
-
-    
-    fmt.Println("Nama Depan saya " + " " + namaDepan)
-    fmt.Println("perkenalkan nama saya " + " " + namaGabungan)
-    
-    // Perhatikan huruf P kapital pada Println
-    fmt.Println("Nama Istri Saya " + " " + nama + " " + samping)
+	fmt.Println("Nama Depan saya" + namaDepan)
+	fmt.Println("perkenalkan nama saya" + namaGabungan)
+	fmt.Println("perhitungan hasil modulo a dan v ", hasil)
+	fmt.Println("angka 10 jika di naikan 1 nilai men jadi angka :", r)
 }
